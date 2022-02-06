b695890f7607129cc39c8912f1e0b1bcd980dbdb
Andreu Mut Portes <amutpor@upv.edu.es>
2022-02-06 18:24:39 +0100

    basic stuff python

---
+++ intro-python/index.py
@@ -0,0 +1,51 @@
+# Esto es un comentario con el hashtag
+if 3 >5:
+    print('Esto no se va a imprimir')
+
+if 5 >3:
+    print('5 es mayor que 3')
+
+x = 275
+y= 'Andrac es mi nombre'
+print(x,y)
+
+email = 'andraquito@lalala.com'
+print(email)
+
+_mi_var ='chanchito'
+mi_var ='chanchito'
+miVar ='chanchito'
+mivar ='chanchito'
+#variables en mayus es para asignar constantes
+#No se pueden asignar variables que empiezen por numero ni guiones. Solo guion bajo.
+MIVAR='Esto es una constante!!!!'
+MIVAR2='otra variables'
+
+#Definir multiples variables en una linea.
+a,b,c= 'lala', 'lele','lili'
+print(a,b,c)
+
+valor1=valor2=valor3='chanchito feliz'
+print(valor1,valor2,valor3)
+
+#Concatenar palabras
+inicio = 'hola'
+final = 'mundo'
+print(inicio + final)
+
+#TIPOS DE DATOS
+#String
+palabra = 'hola mundo'
+oracion = "hola mundo comilla doble"
+
+#Entero
+entero = 20
+#Float
+conDecimales = 20.2
+#Numeros complejos. Se usan muy poco en el curso. Se
+#crean agregando la j
+complejo = 1j
+
+print(palabra,oracion,entero,conDecimales,complejo)
+
+#Listas (o arrays)
