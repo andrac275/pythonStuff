0ae4f6ab65776376a81b5df1db671795a0413407
Andreu Mut <andrac275@gmail.com>
2023-02-14 12:15:17 +0100

    correcciones db y entrade la tarea. Funcion addTodo implementada

--- 000_Ejercicios y proyectos/32_Proyecto_GestorDeTareas/db.py
+++ 000_Ejercicios y proyectos/32_Proyecto_GestorDeTareas/db.py
@@ -13,7 +13,7 @@ c= conn.cursor()
 #Crear la tabla del todo si no existiera
 c.execute("""
     CREATE TABLE if not exists todo(
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         description TEXT NOT NULL,
         completed BOOLEAN NOT NULL
@@ -21,4 +21,28 @@ c.execute("""
 """)
 conn.commit()
 
+def addTodo():
+    todo = e.get()
+    c.execute("""
+        INSERT INTO todo (description, completed) VALUES (?,?)
+    """, (todo, False))
+    conn.commit()
+    e.delete(0,END)
 
+l = Label(root, text="Tarea")
+l.grid(row=0,column=0)
+
+e = Entry(root, width=40)
+e.grid(row=0, column=1)
+
+btn=Button(root, text="Agregar", command= addTodo)
+btn.grid(row=0,column=2)
+
+frame=LabelFrame(root, text="Mis tareas", padx=5, pady=5)
+frame.grid(row=1,column=0, columnspan=3, sticky="nswe",padx=5)
+
+e.focus()
+
+root.bind('<Return>',lambda x : addTodo())
+
+root.mainloop()
