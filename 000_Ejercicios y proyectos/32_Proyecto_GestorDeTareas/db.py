4440c96144ec1353aca0dc0cd94ff619e3b3385f
Andreu Mut <andrac275@gmail.com>
2023-02-14 12:41:57 +0100

    currying aplicado a la funcion complete

--- 000_Ejercicios y proyectos/32_Proyecto_GestorDeTareas/db.py
+++ 000_Ejercicios y proyectos/32_Proyecto_GestorDeTareas/db.py
@@ -21,14 +21,25 @@ c.execute("""
 """)
 conn.commit()
 
+#Currying: es crear una funcion dentro de una funcion para retrasar la ejecucion de la misma
+# Esto guarda el valor del id y devuelve el valor actual y no el ultimo valor del id, que es el
+# que se iteraba en el bucle for que llama a complete  
+def complete(id):
+    #Por convenio se genera el mismo nombre de funcion que la externa pero con una barra baja para 
+    #diferenciar una de la otra y si sale algun error, saber cual de las dos es.
+    def _complete():
+        print(id)
+    return _complete
+
 def render_todos():
     rows = c.execute("SELECT * FROM todo").fetchall()
     print(rows)
 
     for i in range(0, len(rows)):
+        id = rows[i][0]
         completed = rows[i][3] #La cuarta columna (el 3) pone 0 o  1 para saber si está o no está completado
         description = rows[i][2]
-        l=Checkbutton(frame, text = description, width = 42, anchor='w')
+        l=Checkbutton(frame, text = description, width = 42, anchor='w', command = complete(id))
         l.grid(row=i, column = 0, sticky = 'w')
 
 def addTodo():
