86444f1ceea47c479027c3ac3fc82211f01843cc
Andreu Mut <andrac275@gmail.com>
2023-02-19 11:25:06 +0100

    fichero configuracion base de datos e instalacion mysql

--- 000_Ejercicios y proyectos/34_Proyecto_Mailer/app/__init__.py
+++ 000_Ejercicios y proyectos/34_Proyecto_Mailer/app/__init__.py
@@ -14,5 +14,10 @@ def create_app():
         DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
         DATABASE=os.environ.get('FLASK_DATABASE'),
     )
+
+    from . import db
+
+    db.init_app(app)
+
     return app
 
