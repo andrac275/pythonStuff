62bb310198d8855e720102294bffaed55764e754
Andreu Mut <andrac275@gmail.com>
2023-02-21 13:07:13 +0100

    validando los datos de entrada del formulario y estilo

--- 000_Ejercicios y proyectos/34_Proyecto_Mailer/app/mail.py
+++ 000_Ejercicios y proyectos/34_Proyecto_Mailer/app/mail.py
@@ -1,6 +1,8 @@
 from flask import (
     Blueprint,
-    render_template
+    render_template,
+    request,
+    flash
     )
 
 from app.db import get_db
@@ -18,4 +20,22 @@ def index():
 
 @bp.route('/create',methods=['GET','POST'])
 def create():
+    if request.method=='POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors =[]
+
+        if not email:
+            errors.append('Email es obligatorio')
+        if not subject:
+            errors.append('Subject es obligatorio')
+        if not content:
+            errors.append('Contenido es obligatorio')
+        
+        if len(errors)==0:
+            pass
+        else:
+            for error in errors:
+                flash(error)
     return render_template('mails/create.html')
